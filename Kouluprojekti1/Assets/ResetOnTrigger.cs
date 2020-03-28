a6b96bbee83e4d8946999e9dd3cf548f36db15c9
robin1700 <robin1700@student.hamk.fi>
2020-03-28 19:52:04 +0200

    Lisäyksiä labyrinttiin

---
+++ Kouluprojekti1/Assets/ResetOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
---
+++ Kouluprojekti1/Assets/Scenes/Scripts/Rotateobject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotateobject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+            transform.Rotate(Vector3.left * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Rotate(-Vector3.left * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+            Application.LoadLevel(Application.loadedLevel);
+
+    }
+}

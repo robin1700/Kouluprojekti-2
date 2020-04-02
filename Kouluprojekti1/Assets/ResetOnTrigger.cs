e484e3b5267d38b7646a6a831283e8e90a8c1ac7
Juuso1708 <62695354+Juuso1708@users.noreply.github.com>
2020-04-02 14:51:54 +0300

    2 huone lisätty

--- Kouluprojekti1/Assets/ResetOnTrigger.cs
+++ Kouluprojekti1/Assets/ResetOnTrigger.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ResetOnTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnTriggerEnter(Collider other)
-    {
-        Application.LoadLevel(Application.loadedLevel);
-    }
+    Vector3 originalpos;
+
+    //private void Start()
+    //{
+    //    originalpos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+
+    //}
+    //void OnTriggerEnter()
+    //{
+    //    gameObject.transform.position = originalpos;
+    //}
 
 }
